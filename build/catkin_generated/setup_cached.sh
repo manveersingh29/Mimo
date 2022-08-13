88e3a5b6f15e5c224463647d4ab93ddea586bd66
manveersingh29 <singhmanveer2902@gmail.com>
2022-08-13 21:01:23 +0530

    amcl and gmapping

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,7 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/manveer/Mimo/devel:$CMAKE_PREFIX_PATH"
 export PWD='/home/manveer/Mimo/build'
-export ROSLISP_PACKAGE_DIRECTORIES="/home/manveer/Mimo/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
-export ROS_PACKAGE_PATH="/home/manveer/Mimo/src:$ROS_PACKAGE_PATH"
---
+++ build/slam_gmapping/gmapping/CMakeFiles/gmapping-rtest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/manveer/Mimo/devel/lib/gmapping/gmapping-rtest"
+  "/home/manveer/Mimo/devel/lib/gmapping/gmapping-rtest.pdb"
+  "CMakeFiles/gmapping-rtest.dir/test/rtest.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gmapping-rtest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
